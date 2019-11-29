887b4499480ba9186a30f77a80a2e6c37f98fcf8
Johanna Merikanto <johanna.merikanto@outlook.com>
2019-11-29 08:46:45 +0200

    Lisätty responsiivinen sivusto.

---
+++ ResponsiveWebSite/Controllers/ResponsiveSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResponsiveWebSite.Controllers
+{
+    public class ResponsiveSiteController : Controller
+    {
+        // GET: ResponsiveSite
+        public ActionResult RwdSite()
+        {
+            return View();
+        }
+    }
+}
